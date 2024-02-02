e286861fae960b4cb4e17cf7d24c24fe902bae2d
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-02-02 22:02:00 +0100

    feat: implement csv folding function using file folding function

--- src/server/csv.ts
+++ src/server/csv.ts
@@ -67,3 +67,26 @@ export function filter(
     return predicate(record, i - 1);
   });
 }
+
+export function fold<T>(
+  source: string, 
+  destination: string,
+  base: T,
+  reducer: (accumulated: T, record: Record, index: number) => T
+): Promise<T> {
+  const headers: string[] = [];
+  const record: Record = Object.create(null);
+
+  return file.fold(source, destination, base, (accum, line, i) => {
+    const row = line.split(",");
+
+    if(i === 0){
+      row.forEach((header, j) => headers[j] = header);
+      return accum;
+    };
+
+    row.forEach((colVal, i) => record[headers[i]] = colVal);
+
+    return reducer(accum, record, i - 1);
+  });
+}
