276e4041a0557b83caf17be5957f2c0182f145e5
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-02-03 11:50:51 +0100

    updated fold function to make writing result to a file optional

--- src/server/file.ts
+++ src/server/file.ts
@@ -57,24 +57,26 @@ export function forEach(
   });
 }
 
-export function reduce<T>(
+export function fold<T>(
   source: string, 
-  destination: string,
+  destination: string | null,
   base: T,
   reducer: (accumulated: T, line: string, index: number) => T
 ): Promise<T> {
   let i: number = 0;
 
   const readline = createInterface(createReadStream(source, { encoding: "utf-8"}));
-  const wstream = createWriteStream(destination, { encoding: "utf-8"});
-
+  
   readline.on("line", line => {
     base = reducer(base, line, i++);
   }); 
-
+  
   return new Promise((resolve, reject) => {
     readline.on("close", () => {
-      wstream.write(JSON.stringify(base, null, 2));
+      if(destination){
+        const wstream = createWriteStream(destination, { encoding: "utf-8"});
+        wstream.write(JSON.stringify(base, null, 2));
+      }
       resolve(base);
     })
 
