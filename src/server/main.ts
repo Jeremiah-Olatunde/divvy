b3f69022bbbbf8e119c501b1ab33e86ac79fd7e3
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-02-04 21:23:56 +0100

    feat: api call to get number for rides for each month

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import ViteExpress from "vite-express";
+import ViteExpress, { build } from "vite-express";
 
 import * as csv from "./csv.js";
+import { buildArr } from "./utils.js";
 
 const app = express();
 
@@ -14,22 +15,32 @@ const app = express();
 
 const data = "./src/server/dataset/primary.csv";
 
+
+
 app.get("/member-casual-count", async function(_, res){
   const result = await csv.fold(data, null, { casual: [0], member: [0] }, (p, record) => {
     p[record["member_casual"] as "member" | "casual"][0] += 1;
     return p;
-  })
+  });
 
   res.json(JSON.stringify(result));
 });
 
 app.get("/trip-count-month", async function(_, res){
-  /**
-   * return {
-   *  member: [janCount, ..., decCount],
-   *  casual: [janCount, ..., decCount],
-   * }
-  */  
+  const result = await csv.fold(
+    data, 
+    null, 
+    { casual: buildArr(12, _ => 0), member: buildArr(12, _ => 0) },
+    (p, record) => {
+      const status = record["member_casual"] as "member" | "casual";
+      const date = new Date(record["started_at"].split(" ").join("T"));
+      const month = date.getMonth();
+      p[status][month] += 1;
+      return p;
+    }
+  );  
+
+  res.json(JSON.stringify(result));
 });
 
 
