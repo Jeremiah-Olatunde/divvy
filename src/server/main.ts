78f7abdb752453064c0ec23216a39868a714ab04
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-02-04 21:12:38 +0100

    specifications of api for fetching general metrics

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,24 +1,65 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
-import { createReadStream } from "fs";
-import { createInterface } from "readline";
-
-
-const readline = createInterface(createReadStream("./src/server/dataset/random-dataset.csv"));
-let line: string[] = [];
-
-
-readline.on("line", data => {
-  line = data.split(",");
-  readline.pause();
-})
+import * as csv from "./csv.js";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.json(line);
+/**
+ * GENERAL METRICS
+ * total number of (members | casuals)
+ * total number of rides for each (month | year)
+ * total ride durations for each (month | year)
+*/
+
+const data = "./src/server/dataset/primary.csv";
+
+app.get("/member-casual-count", async function(_, res){
+ /**
+  * return {
+  *   member: [count],
+  *   casual: [count],
+  * }
+ */
 });
+
+app.get("/trip-count-month", async function(_, res){
+  /**
+   * return {
+   *  member: [janCount, ..., decCount],
+   *  casual: [janCount, ..., decCount],
+   * }
+  */  
+});
+
+
+app.get("/trip-count-year", async function(_, res){
+  /**
+   * return {
+   *  member: [count],
+   *  casual: [count]
+   * }
+  */
+});
+
+app.get("/trip-duration-month", async function(_, res){
+  /**
+   * return {
+   *  member: [count],
+   *  casual: [count]
+   * }
+  */ 
+});
+
+app.get("/trip-duration-year", async function(_, res){
+  /**
+   * return {
+   *  member: [count],
+   *  casual: [count]
+   * }
+  */
+});
+
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
