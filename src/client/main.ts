6a35ead4fe5b79b2583bc91d45a21805fb5d8b5b
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-02-08 00:03:27 +0100

    feat: member casual ratio pie chart

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,1 +1,38 @@
+import { build } from "vite-express";
 import "./style.css";
+import { Chart } from "chart.js/auto";
+
+
+function buildArr<T>(size: number, getItem: (i: number, xs: T[]) => T): T[] {
+  const rtnArr: T[] = [];
+
+  for(let i = 0; i < size; i++){
+    rtnArr.push(getItem(i, rtnArr));
+  }
+
+  return rtnArr;
+}
+
+type Response = { member: [number], casual: [number] };
+
+
+
+async function pieCasualMembers(){
+  
+  const { member, casual }: Response = JSON.parse(await (await fetch("/trip-count-year")).json());
+  
+  new Chart(document.getElementById("chart") as HTMLCanvasElement, {
+    type: "pie",
+    data: {
+      labels: [ 'casual', 'member' ],
+      datasets: [{
+        label: 'casual vs members',
+        data: [ casual[0], member[0],],
+        backgroundColor: [ 'rgb(255, 99, 132)', 'rgb(54, 162, 235)' ],
+        hoverOffset: 4
+      }]
+    }
+  })
+
+}
+
